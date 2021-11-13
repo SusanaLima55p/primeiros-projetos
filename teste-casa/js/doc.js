c43f455cf4adda1cf708852bde1f687db6cec961
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-12 22:18:44 -0300

    ok

--- teste-casa/js/doc.js
+++ teste-casa/js/doc.js
@@ -2,124 +2,163 @@ var soma =[0, 0, 0, 0,]
 var int = document.getElementById('int')
 var botao = document.getElementById('bu')
 var per1 = document.getElementById('per1')
+var bper1 = document.getElementsByName('bper1')
+var per2 = document.getElementById('per2')
+var bper2 = document.getElementsByName('bper2')
+var per3 = document.getElementById('per3')
+var bper3 = document.getElementsByName('bper3')    
+var per4 = document.getElementById('per4')
+var bper4 = document.getElementsByName('bper4')
+var per5 = document.getElementById('per5')
+var bper5 = document.getElementsByName('bper5')
+var per6 = document.getElementById('per6')
+var bper6 = document.getElementsByName('bper6')
 
 //passagem de pergunta
 function inicio(){
     int.style.display = 'none'
     botao.style.display = 'none'
     per1.style.display = 'block'
+} 
 //segunda pergunta
-    var bper1 = document.getElementsByName('bper1')
-    var prox = document.getElementById('prox')
-    prox.addEventListener('click', proxima)
-    function proxima(){
-        var per2 = document.getElementById('per2')
+function proxima(){
+    
+    if (bper1[0].checked){
+        soma[0] +1.2
         per1.style.display = 'none'
         per2.style.display = 'block'
-        var bper2 = document.getElementsByName('bper2')
-        // terceira pergunta
-        var prox2 = document.getElementById('prox2')
-        prox2.addEventListener('click', proxima2)
-        function proxima2(){
-            var per3 = document.getElementById('per3')
-            per2.style.display = 'none'
-            per3.style.display = 'block'
-            var bper3 = document.getElementsByName('bper3')
-            //quarta pergunta
-            var prox3 = document.getElementById('prox3')
-            prox3.addEventListener('click', proxima3)
-            function proxima3(){
-                var per4 = document.getElementById('per4')
-                per3.style.display = 'none'
-                per4.style.display = 'block'
-                var bper4 = document.getElementsByName('bper4')
-                //quinta pergunta
-                var prox4 = document.getElementById('prox4')
-                prox4.addEventListener('click', proxima4)
-                function proxima4(){
-                    var per5 = document.getElementById('per5')
-                    per4.style.display = 'none'
-                    per5.style.display = 'block'
-                    var bper5 = document.getElementsByName('bper5')
-                    //sexta pergunta
-                    var prox5 = document.getElementById('prox5')
-                    prox5.addEventListener('click', proxima5)
-                    function proxima5(){
-                        var per6 = document.getElementById('per6')
-                        per5.style.display = 'none'
-                        per6.style.display = 'block'
-                        var bper6 = document.getElementsByName('bper6')
-//verificação
-var ver = document.getElementById('ver')
-ver.addEventListener('click', verificar)
+    } else if (bper1[1].checked){
+        soma[1] +1.2
+        per1.style.display = 'none'
+        per2.style.display = 'block'
+    } else  if (bper1[2].checked){        
+        soma[2] +1.2
+        per1.style.display = 'none'
+        per2.style.display = 'block'
+    } else if (bper1[3].checked){
+        soma[3] +1.2
+        per1.style.display = 'none'
+        per2.style.display = 'block'
+    } else{
+    alert('Selecione uma opção!')
+    }
+}
+    
+    
 
-    //contagem de ponto primeira pergunta
-    if (bper1[0].checked){
-        soma[0] ++
-    } if (bper1[1].checked){
-        soma[1] ++
-    } if (bper1[2].checked){        
-        soma[2] ++
-    } if (bper1[3].checked){
-        soma[3] ++    
-    }
-    // segunda pergunta
+// terceira pergunta 
+function proxima2(){
     if (bper2[0].checked){
-        soma[2] ++
+        soma[2] +1.3
+        per2.style.display = 'none'
+        per3.style.display = 'block'
     } else if (bper2[1].checked){
-        soma[1] ++
+        soma[1] +1.3
+        per2.style.display = 'none'
+        per3.style.display = 'block'
     } else if (bper2[2].checked){        
-        soma[3] ++
+        soma[3] +1.3
+        per2.style.display = 'none'
+        per3.style.display = 'block'
     } else if (bper2[3].checked){
-        soma[0] ++
-    }
-    //terceira pergunta
+        soma[0] +1.3
+        per2.style.display = 'none'
+        per3.style.display = 'block'
+    }else{
+        alert('Selecione uma opção!') 
+    } 
+}
+//quarta pergunta
+function proxima3(){
     if (bper3[0].checked){
-        soma[1] += 2
+        soma[1] += 1.7
+        per3.style.display = 'none'
+        per4.style.display = 'block'
     } else if (bper3[1].checked){
-        soma[2] += 2
+        soma[2] += 1.7
+        per3.style.display = 'none'
+        per4.style.display = 'block'
     } else if (bper3[2].checked){        
-        soma[3] += 2
+        soma[3] += 1.7
+        per3.style.display = 'none'
+        per4.style.display = 'block'
     } else if (bper3[3].checked){
-        soma[0] += 2
-    }
-    //quarta pergunta
+        soma[0] += 1.7
+        per3.style.display = 'none'
+        per4.style.display = 'block'
+    }else{
+        alert('Selecione uma opção!') 
+    }   
+}   
+//quinta pergunta
+function proxima4(){
     if (bper4[0].checked){
-        soma[2] ++
+        soma[2] +1.6
+        per4.style.display = 'none'
+        per5.style.display = 'block'
     } else if (bper4[1].checked){
-        soma[0] ++
+        soma[0] +1.6
+        per4.style.display = 'none'
+        per5.style.display = 'block'
     } else if (bper4[2].checked){        
-        soma[1] ++
+        soma[1] +1.6
+        per4.style.display = 'none'
+        per5.style.display = 'block'
     } else if (bper4[3].checked){
-        soma[3] ++
-    }
-    //quinta pergunta
+        soma[3] +1.6
+        per4.style.display = 'none'
+        per5.style.display = 'block'
+    }else{
+        alert('Selecione uma opção!') 
+    } 
+}
+//sexta pergunta
+function proxima5(){
     if (bper5[0].checked){
-        soma[2] ++
+        soma[2] +1.5
+        per5.style.display = 'none'
+        per6.style.display = 'block'
     } else if (bper5[1].checked){
-        soma[0] ++
+        soma[0] +1.5
+        per5.style.display = 'none'
+        per6.style.display = 'block'
     } else if (bper5[2].checked){        
-        soma[1] ++
+        soma[1] +1.5
+        per5.style.display = 'none'
+        per6.style.display = 'block'
     } else if (bper5[3].checked){
-        soma[3] ++
+        soma[3] +1.5
+        per5.style.display = 'none'
+        per6.style.display = 'block'
+    } else{
+        alert('Selecione uma opção!') 
     }
-    //sexta pergunta
+}
+//verificação
+    
+var casa = document.getElementById('casa')
+var img = document.getElementById('emb')
+function verificar(){
     if (bper6[0].checked){
         soma[0] ++
+        resultado()
     } else if (bper6[1].checked){
         soma[1] ++
+        resultado()
     } else if (bper6[2].checked){        
         soma[3] ++
+        resultado()
     } else if (bper6[3].checked){
         soma[2] ++
-    }
-    
-function verificar(){
-    per6.style.display = 'none'
-    var casa = document.getElementById('casa')
-    var img = document.getElementById('emb')
+        resultado()
+    } else{
+        alert('Selecione uma opção!')
+    } 
+}
 
-    
+
+function resultado(){
+    per6.style.display = 'none'
     //grifinoria
     if (soma[0] > soma[3] + soma[2] + soma[1]){
         casa.innerHTML = `Parabéns você foi selecionado para a casa <mark id="gri">Grifinória!</mark>`
@@ -140,13 +179,5 @@ function verificar(){
         casa.innerHTML = `Parabéns você foi selecionado para a casa <mark id="son">Sonserina!</mark>`
         document.body.style.background = '#0b362f'
         img.src = 'imagens/son.png'
-
     }
-
-}
-}
-} 
-}
-}
-}
 }
