ade8d37cf9f0fad13387de66dbacd46cfdcd012f
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-15 22:08:54 -0300

    ok

--- calculadora/js/doc.js
+++ calculadora/js/doc.js
@@ -7,80 +7,96 @@ function menu(){
 }
 function um(){
     vis.value += 1
+    resu.innerHTML += '1'
 }
 function dois(){
     vis.value += 2
+    resu.innerHTML += '2'
 }
 function tres(){
     vis.value += 3
+    resu.innerHTML += '3'
 }
 function quatro(){
     vis.value += 4
+    resu.innerHTML += '4'
 }
 function cinco(){
     vis.value += 5
+    resu.innerHTML += '5'
 }
 function seis(){
     vis.value += 6
+    resu.innerHTML += '6'
 }
 function sete(){
     vis.value += 7
+    resu.innerHTML += '7'
 }
 function oito(){
     vis.value += 8
+    resu.innerHTML += '8'
 }
 function nove(){
     vis.value += 9
+    resu.innerHTML += '9'
 }
 function zero(){
     vis.value += 0
+    resu.innerHTML += '0'
 }
 function mais(){
     nu1[0] = vis.value
+    resu.innerHTML = '+'
     sinal = '+'
     vis.value = ``
 }
 function menos(){
     nu1[0] = vis.value
+    resu.innerHTML = '-'
     sinal = '-'
     vis.value = ``
 }
 function mult(){
     nu1[0] = vis.value
+    resu.innerHTML = '*'
     sinal = '*'
     vis.value = ``
 }
 function divi(){
     nu1[0] = vis.value
+    resu.innerHTML = '/'
     sinal = '/'
     vis.value = ``
 }
 function del(){
     vis.value = ``
+    resu.innerHTML = ''
 }
+
 function igual(){
-    if(vis.value.length == 0){
+    if(nu1.length == 0){
         alert('digite sua operação')
-    }
+    } else{
     nu1[1] = vis.value
-    
+    vis.value = ''
+    let res = 0
     var n1 = Number(nu1[0])
     var n2 = Number(nu1[1])
-    let res = 0 
-    vis.value = ''
-    
-    if(sinal == '+'){
-        res = (n1 + n2)
-    } else if(sinal == '-'){
-        res = (n1 - n2)
-    } else if(sinal == '*'){
-        res = (n1 * n2)
-    } else if(sinal == '/'){
-        if(n1 == 0 || n2 == 0 ){
-            res = 0
-        }else {
-        res = (n1 / n2)
+        if(sinal == '+'){
+            res = (n1 + n2)
+        } else if(sinal == '-'){
+            res = (n1 - n2)
+        } else if(sinal == '*'){
+            res = (n1 * n2)
+        } else if(sinal == '/'){
+            if(n1 == 0 || n2 == 0 ){
+                res = 0
+            }else {
+            res = (n1 / n2)
+            }
         }
+        vis.value = res
+        resu.innerHTML = `${res}`
     }
-    vis.value = res
 }
