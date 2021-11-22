cb395415ee5a70ed3eb298542d5b26ecde781b46
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-22 11:16:34 -0300

    correcao de menu

--- estacao/js/doc.js
+++ estacao/js/doc.js
@@ -1,21 +1,27 @@
+
 let men = document.getElementById('menu')
 let prin = document.getElementById('prin')
 let pes = document.getElementById('pes')
 let link = document.getElementById('link')
+let barrame = document.getElementById('barrame')
 let cli = ''
 let op = ''
 function menu(){
+    
     if(cli == 'ativo'){
         men.style.display = 'none'
         men.style.position = 'none'
         prin.style.margin = '0px'
         prin.style.margin = '30px 0px'
+        barrame.style.margin = '-86px 0px 0px 0px'
         cli = ''
     } else {
         cli = 'ativo'
         men.style.display = 'grid'
-        men.style.position = 'relative'
-        prin.style.margin = '-270px 0px 30px 0px'
+        men.style.margin ='-30px 0px 0px 0px'
+        prin.style.margin = '0px 0px 30px 0px'
+        barrame.style.margin = '-86px 0px 0px 0px'
+        link.style.margin ='-140px 0px 0px 0px'
     }
 }
 
